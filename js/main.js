a69ea8da014c0ec3390b3435acd48ef7835347ff
Yinpu Li <yinpuli@icloud.com>
2022-10-06 21:06:28 -0700

    Revert "Update email and github page."
    This reverts commit cb83170b0b4a3621ed49216dcc16ae62fa6e8429.

--- js/main.js
+++ js/main.js
@@ -28,8 +28,8 @@
           return;
         }
 
-        var emailName = 'seungchoi1850';
-        var emailDomain = 'gmail.com';
+        var emailName = 'antonio.linero';
+        var emailDomain = 'austin.utexas.edu';
         var $email = $(
           '<span id="email">' +
             '<a href="mailto:' +
